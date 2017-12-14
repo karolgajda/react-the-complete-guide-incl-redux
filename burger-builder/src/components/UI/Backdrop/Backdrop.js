8f66ec1b8910c556a1bc836040e489f866e79fd4
kgajda <karoll.gajda@gmail.com>
2017-12-14 13:53:07 +0100

    Implementing the Backdrop Component

--- burger-builder/src/components/UI/Backdrop/Backdrop.js
+++ burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import classes from './Backdrop.css';
-import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey}/>;
-            })
-        })
-        .reduce((arr, el) => {
-            return arr.concat(el);
-        }, []);
+const backdrop = (props) => (
+    props.show ? <div className={classes.Backdrop} onClick={props.click}/> : null
+);
 
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>
-    }
-
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
-    );
-};
-
-export default burger;
+export default backdrop;
